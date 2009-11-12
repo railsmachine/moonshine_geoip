e0f2462dfc56216d50e19a7b5b26bcd7dd15af35
Ches Martin <ches@whiskeyandgrits.net>
2009-11-12 22:15:33 +0700

    Initial version

--- lib/geoip.rb
+++ lib/geoip.rb
@@ -1,19 +1,95 @@
 module Geoip
+  GEOIP_TEMPLATES_DIR = File.join(File.dirname(__FILE__), '..', 'templates')
 
   # Define options for this plugin via the <tt>configure</tt> method
   # in your application manifest:
   #
-  #   configure(:geoip => {:foo => true})
+  #   configure(:geoip => {:user_id => 'Joebob'})
   #
   # Then include the plugin and call the recipe(s) you need:
   #
-  #  plugin :geoip
-  #  recipe :geoip
-  def geoip(options = {})
-    # define the recipe
-    # options specified with the configure method will be 
-    # automatically available here in the options hash.
-    #    options[:foo]   # => true
+  #   plugin :geoip
+  #   recipe :geoip, :mod_geoip
+  #
+  # New Ubuntu versions have a <tt>geoip-database</tt> package, but MaxMind
+  # updates the DB monthly, and I'm not sure the package will keep up. There
+  # is also a built-in program to grab updates, *if* you have a license (see
+  # <tt>man geoipupdate</tt>) -- this is used if you configure account
+  # credentials, otherwise cron will be set up to fetch updates manually.
+  #
+  # The following options are configurable:
+  #   * +geo_database_url+
+  #   * +user_id+
+  #   * +license_key+
+  #   * +product_ids+
+  # Most apply to licensees of paid GeoIP databases -- see {this page}[http://www.maxmind.com/app/license_key]
+  # for information on how these are used. You can also specify the URL of the
+  # database you wish to download and update -- default is 'GeoLite Country'.
+  def geoip(opts={})
+    options = {
+      :geo_database_url => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz'
+    }.merge!(opts)
+
+    package 'geoip-bin', :ensure => :installed, :notify => exec('new-geoip-db')
+
+    db_update_command = nil
+
+    # Prefer to use the built-in +geoipupdate+ tool if possible, configuring
+    # GeoIP for a licensed account if credentials are given.
+    if options[:user_id] && options[:license_key]
+      db_update_command = '/usr/bin/geoipupdate'
+
+      file '/etc/GeoIP.conf',
+        :ensure => :present,
+        :mode => 644,
+        :before => exec('new-geoip-db'),
+        :content => template(File.join(GEOIP_TEMPLATES_DIR, 'etc-geoip.conf.erb'), binding)
+
+      # First-time update
+      exec 'new-geoip-db',
+        :command => db_update_command,
+        :require => package('geoip-bin'),
+        :refreshonly => true
+    else
+      package 'wget', :ensure => :installed
+      file '/usr/local/share/GeoIP', :ensure => :directory
+      db_update_command =
+        [
+          "cd /tmp",
+          "/usr/bin/wget #{options[:geo_database_url]}",
+          "/bin/gunzip GeoIP.dat.gz",
+          "/bin/mv -f GeoIP.dat /usr/local/share/GeoIP"
+        ].join(' && ')
+
+      # First-time update
+      # An explicit +cd+ is done in the command rather than using a ShadowPuppet
+      # +:cwd+ parameter so that it applies for cron job usage as well
+      exec 'new-geoip-db',
+        :command => db_update_command,
+        :creates => '/usr/local/share/GeoIP/GeoIP.dat',
+        :require => [
+          package('wget'),
+          file('/usr/local/share/GeoIP')
+        ]
+    end
+
+    # Updates are released on the first of every month
+    cron 'Monthly GeoIP database updates',
+      :user => 'root',
+      :command => db_update_command,
+      :minute => 33, :hour => 3, :monthday => 2
   end
-  
+
+  # Configure and enable the Apache mod_geoip module
+  def mod_geoip(options={})
+    package 'libapache2-mod-geoip', :ensure => :installed
+    a2enmod 'geoip'
+    file '/etc/apache2/mods-available/geoip.conf',
+      :ensure => :present,
+      :mode => '644',
+      :content => template(File.join(GEOIP_TEMPLATES_DIR, 'geoip.conf.erb'), binding),
+      :before => exec('a2enmod geoip'),
+      :notify => service('apache2')
+  end
 end
+
--- spec/geoip_spec.rb
+++ spec/geoip_spec.rb
@@ -1,24 +1,129 @@
 require File.join(File.dirname(__FILE__), 'spec_helper.rb')
 
-class GeoipManifest < Moonshine::Manifest
+class GeoipManifest < Moonshine::Manifest::Rails
   plugin :geoip
 end
 
 describe "A manifest with the Geoip plugin" do
-  
   before do
     @manifest = GeoipManifest.new
-    @manifest.geoip
   end
-  
-  it "should be executable" do
-    @manifest.should be_executable
+
+  shared_examples_for "default behavior" do
+    before do
+      @manifest.geoip
+    end
+
+    it 'should install the geoip-bin package' do
+      @manifest.packages.keys.should include 'geoip-bin'
+    end
+
+    it 'should use the GeoLite Country database' do
+      @manifest.execs['new-geoip-db'].command.should match(/GeoLiteCountry/)
+    end
+
+    it 'should create GeoIP.dat under /usr/local' do
+      @new_db_exec = @manifest.execs['new-geoip-db']
+      @new_db_exec.command.should match(/\/usr\/local\/share/)
+      @new_db_exec.creates.should match(/\/usr\/local\/share/)
+    end
+
+    it 'should not create /etc/GeoIP.conf' do
+      @manifest.files.keys.should_not include '/etc/GeoIP.conf'
+    end
+
+    it 'should create cron job updating database in /usr/local' do
+      cron_job = @manifest.crons['Monthly GeoIP database updates']
+      cron_job.command.should match(/\usr\/local\/share\/GeoIP/)
+    end
+
+    describe "and using the mod_geoip recipe" do
+      before do
+        # FIXME: this shouldn't be needed and it breaks the it_should_behave_like
+        # cases that need specific configuration
+        @manifest.configure(:geoip => {})
+        @manifest.mod_geoip
+      end
+
+      it 'should use /usr/local database in the module conf' do
+        conf_file = @manifest.files['/etc/apache2/mods-available/geoip.conf']
+        conf_file.content.should match(/GeoIPDBFile \/usr\/local\/share/)
+      end
+
+    end
+
   end
-  
-  #it "should provide packages/services/files" do
-  # @manifest.packages.keys.should include 'foo'
-  # @manifest.files['/etc/foo.conf'].content.should match /foo=true/
-  # @manifest.execs['newaliases'].refreshonly.should be_true
-  #end
-  
+
+  describe "with no options" do
+    it_should_behave_like "default behavior"
+  end
+
+  describe "with options for user_id and license_key" do
+    before do
+      @manifest.geoip(:user_id => 'Bob', :license_key => 'tiddlywinks')
+    end
+
+    it 'should install the geoip-bin package' do
+      @manifest.packages.keys.should include 'geoip-bin'
+    end
+
+    it 'should create /etc/GeoIP.conf' do
+      @manifest.files.keys.should include '/etc/GeoIP.conf'
+      @manifest.files['/etc/GeoIP.conf'].content.should match(/tiddlywinks/)
+    end
+
+    it 'should use built-in geoipupdate tool for DB setup and updates' do
+      @manifest.execs['new-geoip-db'].command.should match(/geoipupdate/)
+      cron_job = @manifest.crons['Monthly GeoIP database updates']
+      cron_job.command.should match(/geoipupdate/)
+    end
+
+    describe "and using the mod_geoip recipe" do
+      before do
+        @manifest.configure(:geoip => {:user_id => 'Jimbob', :license_key => 'tiddlywinks'})
+        @manifest.mod_geoip
+      end
+
+      it 'should use /usr/share database in the module conf' do
+        conf_file = @manifest.files['/etc/apache2/mods-available/geoip.conf']
+        conf_file.content.should match(/GeoIPDBFile \/usr\/share/)
+      end
+
+    end
+
+  end
+
+  describe "with custom database updates URL" do
+    before do
+      @manifest.geoip(:geo_database_url => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz')
+    end
+
+    it 'should use the custom URL for first update' do
+      @manifest.execs['new-geoip-db'].command.should match(/GeoLiteCity/)
+    end
+
+    it 'should use the custom URL for cron updates' do
+      @manifest.crons['Monthly GeoIP database updates'].command.should match(/GeoLiteCity/)
+    end
+
+  end
+
+  describe "with only the user_id option set" do
+    before do
+      @manifest.configure(:geoip => {:user_id => 'Joebob'})
+      @manifest.geoip()
+    end
+
+    it_should_behave_like "default behavior"
+  end
+
+  describe "with only the license_key option set" do
+    before do
+      @manifest.geoip(:license_key => 'tiddlywinks')
+    end
+
+    it_should_behave_like "default behavior"
+  end
+
 end
+
